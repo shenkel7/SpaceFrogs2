af80fc0576938d13e8366a7967890b1772fe6b55
Kelly Shen <kellyshenfrog@gmail.com>
2022-01-08 00:29:35 -0600

    setting up main frame

---
+++ Assets/Team4Frogkid2/Scripts/Button.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    [SerializeField]
+    SweatsManager sweats;
+
+    [SerializeField]
+    Animator animator;
+
+    bool isWin = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetIsWin(bool w)
+    {
+        isWin = w;
+    }
+
+    public void SetButtonAnim(int i)
+    {
+        animator.SetInteger("button", i);
+    }
+
+    private void OnMouseDown()
+    {
+        animator.SetBool("pressed", true);
+        sweats.CheckCondition(isWin);
+    }
+}
---
+++ Assets/Team4Frogkid2/Scripts/SweatsManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SweatsManager : MonoBehaviour
+{
+
+    [SerializeField]
+    List<Button> buttons;
+
+    [SerializeField]
+    private List<int> buttonSelector;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        MinigameManager.Instance.minigame.gameWin = false;
+        int winId = Random.Range(0, buttons.Count);
+
+
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            if (i != winId)
+            {
+                // select a sprite at random
+                if(buttonSelector.Count > 0)
+                {
+                    int index = Random.Range(0, buttonSelector.Count);
+                    int p = buttonSelector[index];
+                    buttonSelector.RemoveAt(index);
+                    buttons[i].SetButtonAnim(p);
+                }
+                else
+                {
+                    // safety fallback
+                    buttons[i].SetButtonAnim(0);
+                }
+            }
+            else
+            {
+                buttons[i].SetButtonAnim(0);
+                buttons[winId].SetIsWin(true);
+            }
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckCondition(bool isWin)
+    {
+        if(isWin)
+        {
+            Win();
+        } else
+        {
+            Lose();
+        }
+    }
+
+    void Win()
+    {
+        Debug.Log("win");
+    }
+
+    void Lose()
+    {
+        Debug.Log("lose");
+    }
+}
